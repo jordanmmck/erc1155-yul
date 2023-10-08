f6d5d56b7e9447fc1012d434664cc77164cf9a9a
Jordan McKinney <jordanmmck@gmail.com>
2023-10-08 12:16:30 -0700

    init

--- test/lib/YulDeployer.sol
+++ test/lib/YulDeployer.sol
@@ -9,7 +9,9 @@ contract YulDeployer is Test {
     ///@param fileName - The file name of the Yul contract. For example, the file name for "Example.yul" is "Example"
     ///@return deployedAddress - The address that the contract was deployed to
     function deployContract(string memory fileName) public returns (address) {
-        string memory bashCommand = string.concat('cast abi-encode "f(bytes)" $(solc --yul yul/', string.concat(fileName, ".yul --bin | tail -1)"));
+        string memory bashCommand = string.concat(
+            'cast abi-encode "f(bytes)" $(solc --strict-assembly yul/', string.concat(fileName, ".yul --bin | tail -1)")
+        );
 
         string[] memory inputs = new string[](3);
         inputs[0] = "bash";
@@ -25,10 +27,7 @@ contract YulDeployer is Test {
         }
 
         ///@notice check that the deployment was successful
-        require(
-            deployedAddress != address(0),
-            "YulDeployer could not deploy contract"
-        );
+        require(deployedAddress != address(0), "YulDeployer could not deploy contract");
 
         ///@notice return the address that the contract was deployed to
         return deployedAddress;
